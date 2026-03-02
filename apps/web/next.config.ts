5f1aa305a572179dc5d06eddc0bf33f4eaba3cb3
marceloamellopaixao <marceloamellopaixao@gmail.com>
2026-03-02 13:52:22 -0300

    feat(config): simplificar padrões remotos de imagens para permitir qualquer hostname

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -11,14 +11,7 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "cdn.discordapp.com", port: "", pathname: "/**" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com", port: "", pathname: "/**" },
-      { protocol: 'https', hostname: 'images.igdb.com', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'shared.akamai.steamstatic.com', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'cdn2.steamgriddb.com', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'cdn3.steamgriddb.com', port: '', pathname: '/**', },
-      { protocol: 'https', hostname: 'shared.akamai.steamstatic.com', port: '', pathname: '/**', },
-      { protocol: 'https', hostname: 'avatars.steamstatic.com', port: '', pathname: '/**', },
+      { protocol: "https", hostname: "**" }
     ],
   },
 };
